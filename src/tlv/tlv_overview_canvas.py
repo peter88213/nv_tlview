da170c9c65b0f7cf3530422f5f0ea273449b2a84
peter88213 <petert88213@gmail.com>
2025-06-20 09:52:06 +0200

    Updated docstrings.

--- src/tlv/tlv_overview_canvas.py
+++ src/tlv/tlv_overview_canvas.py
@@ -1,7 +1,7 @@
 """Provide a tk canvas for a large-scale timeline overview.
 
 Copyright (c) 2025 Peter Triesberger
-For further information see https://github.com/peter88213/
+For further information see https://github.com/peter88213/nv_tlview
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from _datetime import date
