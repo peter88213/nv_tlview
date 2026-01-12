10dfbacb7df49e421bc1d7200511c352f87158ad
peter88213 <petert88213@gmail.com>
2026-01-12 13:16:08 +0100

    Updated the copyright notice.

--- src/nv_tlview.py
+++ src/nv_tlview.py
@@ -1,7 +1,7 @@
 """A timeline view plugin for novelibre.
 
 Requires Python 3.7+
-Copyright (c) 2025 Peter Triesberger
+Copyright (c) Peter Triesberger
 For further information see https://github.com/peter88213/nv_tlview
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 
--- src/tlv/tlv_main_frame.py
+++ src/tlv/tlv_main_frame.py
@@ -1,6 +1,6 @@
 """Provide a tkinter widget for a TlvMainFrame view.
 
-Copyright (c) 2025 Peter Triesberger
+Copyright (c) Peter Triesberger
 For further information see https://github.com/peter88213/nv_tlview
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
